2358899d5060417eecf2957e066830383e772def
Ilman <44839852+ilmndwntr@users.noreply.github.com>
2026-01-08 19:34:32 +0700

    Update qbx_core.sql

--- qbx_core.sql
+++ qbx_core.sql
@@ -1,51 +1,61 @@
 CREATE TABLE IF NOT EXISTS `players` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `citizenid` varchar(50) NOT NULL,
-  `cid` int(11) DEFAULT NULL,
-  `license` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `money` text NOT NULL,
-  `charinfo` text DEFAULT NULL,
-  `job` text NOT NULL,
-  `gang` text DEFAULT NULL,
-  `position` text NOT NULL,
-  `metadata` text NOT NULL,
-  `inventory` longtext DEFAULT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `citizenid` VARCHAR(50) NOT NULL,
+  `cid` INT DEFAULT NULL,
+  `license` VARCHAR(255) NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
+  `money` TEXT NOT NULL,
+  `charinfo` TEXT,
+  `job` TEXT NOT NULL,
+  `gang` TEXT,
+  `position` TEXT NOT NULL,
+  `metadata` TEXT NOT NULL,
+  `inventory` LONGTEXT,
   `phone_number` VARCHAR(20) DEFAULT NULL,
-  `last_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `last_updated` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`citizenid`),
   KEY `id` (`id`),
   KEY `last_updated` (`last_updated`),
   KEY `license` (`license`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 
 ALTER TABLE `players`
-ADD COLUMN IF NOT EXISTS `last_logged_out` timestamp NULL DEFAULT NULL AFTER `last_updated`,
-MODIFY COLUMN `name` varchar(255) NOT NULL COLLATE utf8mb4_unicode_ci;
+ADD COLUMN `last_logged_out` TIMESTAMP NULL DEFAULT NULL;
+
 
 ALTER TABLE `players`
-ADD COLUMN IF NOT EXISTS `userId` INT UNSIGNED DEFAULT NULL AFTER `id`;
+ADD COLUMN `userId` INT UNSIGNED DEFAULT NULL;
+
+ALTER TABLE `players`
+MODIFY COLUMN `name` VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `bans` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) DEFAULT NULL,
-  `license` varchar(50) DEFAULT NULL,
-  `discord` varchar(50) DEFAULT NULL,
-  `ip` varchar(50) DEFAULT NULL,
-  `reason` text DEFAULT NULL,
-  `expire` int(11) DEFAULT NULL,
-  `bannedby` varchar(255) NOT NULL DEFAULT 'LeBanhammer',
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50) DEFAULT NULL,
+  `license` VARCHAR(50) DEFAULT NULL,
+  `discord` VARCHAR(50) DEFAULT NULL,
+  `ip` VARCHAR(50) DEFAULT NULL,
+  `reason` TEXT,
+  `expire` INT DEFAULT NULL,
+  `bannedby` VARCHAR(255) NOT NULL DEFAULT 'LeBanhammer',
   PRIMARY KEY (`id`),
   KEY `license` (`license`),
   KEY `discord` (`discord`),
   KEY `ip` (`ip`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 
 CREATE TABLE IF NOT EXISTS `player_groups` (
   `citizenid` VARCHAR(50) NOT NULL,
   `group` VARCHAR(50) NOT NULL,
   `type` VARCHAR(50) NOT NULL,
-  `grade` TINYINT(3) UNSIGNED NOT NULL,
+  `grade` TINYINT UNSIGNED NOT NULL,
   PRIMARY KEY (`citizenid`, `type`, `group`),
-  CONSTRAINT `fk_citizenid` FOREIGN KEY (`citizenid`) REFERENCES `players` (`citizenid`) ON UPDATE CASCADE ON DELETE CASCADE
+  CONSTRAINT `fk_player_groups_citizenid`
+    FOREIGN KEY (`citizenid`)
+    REFERENCES `players` (`citizenid`)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
